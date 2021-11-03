0649c59e4b1c095209b07768f6fd3696317550f8
thomas-nivot <thomas_nivot@hotmail.fr>
2021-11-03 02:52:48 +0100

    add the function

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
-const array = [30, 15, 5, 20, -20, -1, 10, -5, 3, 2];
-let result = null;
+function nearZero(array) {
+  let result = null;
 
-for (const value of array) {
-  if (result === null || Math.abs(value) <= Math.abs(result)) {
-    result = value;
+  for (const value of array) {
+    if (result === null || Math.abs(value) <= Math.abs(result)) {
+      result = value;
+    }
   }
+  return array.lenth === 0 ? "0" : result;
 }
-
-console.log(array.lenth === 0 ? "0" : result);
+const array = [30, 15, 5, 20, -20, -1, 10, -5, 3, 2];
+console.log(nearZero(array));
--- test.php
+++ test.php
@@ -1,12 +1,17 @@
 <?php
 
+function nearZero($array)
+{
+    $result = null;
+
+    foreach ($array as $value) {
+        if ($result === null || abs($value) <= abs($result)) {
+            $result = $value;
+        }
+    };
+
+    return count($array) === 0 ? '0' : $result;
+}
+
 $array = array(30, 15, 5, 20, -20, -1, 10, -5, 3, 2);
-$result = null;
-
-foreach ($array as $value) {
-    if ($result === null || abs($value) <= abs($result)) {
-        $result = $value;
-    }
-};
-
-echo (count($array) === 0 ? '0' : $result);
+echo (nearZero($array));
